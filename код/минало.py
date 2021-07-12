9e83478fd1f0582b70eb174628568c198aaf413b
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2021-07-12 22:05:58 +0300

    Автоматично запазвам локално променени ['код/минало.py']

--- код/минало.py
+++ код/минало.py
@@ -345,7 +345,7 @@ def минута(username, host, port):
         if fellow['id'] not in remotes:
             glog.info(git.remote.add(fellow['id'], remote))
         else:
-            print('IMHERE')
+            print('IMHERE', fellow['id'], remote)
             glog.info(git.remote('set-url', fellow['id'], remote))
         try:
             pull = git.pull('--ff-only', fellow['id'], 'main')
